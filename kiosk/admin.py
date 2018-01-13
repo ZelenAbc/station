d021bd3d76114554babae39d3f68ed7766b4f86f
Zelen <zelen.abc@gmail.com>
2018-01-13 15:51:54 +0300

    First step of DB creation

--- kiosk/admin.py
+++ kiosk/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Sale, Provider, Ingredient, FiscalCheck
 
 admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(Provider)
+admin.site.register(Ingredient)
+admin.site.register(FiscalCheck)
--- kiosk/models.py
+++ kiosk/models.py
@@ -1,13 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 
-class Product(models.Model):
+class Provider(models.Model):
     name = models.CharField(max_length=200)
-    cost = models.IntegerField(default=0)
-
-    def publish(self):
-        self.save()
 
     def __str__(self):
         return self.name
+
+
+class Consumables(models.Model):
+    name = models.CharField(max_length=200)
+    size = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Offer(models.Model):
+    consumables = models.ForeignKey(Consumables, on_delete=models.CASCADE)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, null=False)
+    cost = models.FloatField(default=0)
+    is_actual = models.BooleanField(default=True)
+
+
+class Product(models.Model):  # products that has a button in tha application
+    name = models.CharField(max_length=200)
+    cost = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def get_cost_price(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    consumables = models.ForeignKey(Consumables, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.consumables)
+
+
+class FiscalCheck(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    check_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.seller) + " " + str(self.check_time)
+
+
+class Sale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+    count = models.SmallIntegerField(default=0, null=False)
+
+    fiscal_check = models.ForeignKey(FiscalCheck, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.product) + " " + str(self.count)
+
